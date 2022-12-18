2b0ea86e9a8d88085bb4aa2e54235861042ba196
saymanrabbi <mdrabbi.asm@gmail.com>
2022-12-18 19:32:00 +0600

    card design

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -6,7 +6,6 @@ import { setLaunches } from "../features/launches/launchesSlices";
 const Home = () => {
   const dispatch = useDispatch();
   const launches = useSelector((state: any) => state.launches.launches);
-  console.log(launches);
   useEffect(() => {
     fetch("https://api.spacexdata.com/v3/launches")
       .then((res) => res.json())
@@ -15,26 +14,38 @@ const Home = () => {
       });
   }, []);
   return (
-    <div>
+    <div style={{ marginTop: "50px" }}>
       <Row style={{ width: "80%", margin: "0 auto" }} gutter={10}>
         {launches.map((launch: any) => (
           <Col span={8}>
             <Card
+              style={{
+                borderRadius: "10px",
+                border: "1px solid #ccc",
+                margin: "5px 0px",
+                padding: "10px",
+              }}
               cover={
                 <img
                   style={{
-                    width: "100%",
-                    height: "100%",
+                    width: "50%",
+                    height: "50%",
                     objectFit: "cover",
                     backgroundPosition: "center",
+                    margin: "0 auto",
                   }}
                   src={launch.links.mission_patch_small}
                 />
               }
             >
               <Card.Meta
+                style={{ textAlign: "center", padding: "0 10px" }}
                 title={launch.mission_name}
-                description={launch.details?.slice(0, 30) + "..."}
+                description={`${
+                  launch?.details?.length > 20
+                    ? launch.details?.slice(0, 30)
+                    : launch.details || "No Details"
+                }...`}
               />
             </Card>
           </Col>
