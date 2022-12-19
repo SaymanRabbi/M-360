3c776e216d09f7961c7f814065b9e30a64bf14f0
saymanrabbi <mdrabbi.asm@gmail.com>
2022-12-19 13:50:37 +0600

    changes

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,5 +1,5 @@
 import type { MenuProps } from "antd";
-import { Button, Card, Col, Dropdown, Row } from "antd";
+import { Button, Card, Col, Row } from "antd";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setStatus, setUpcoming } from "../features/launches/FilterSlices";
@@ -30,6 +30,162 @@ const Home = () => {
       label: <span>Last Year</span>,
     },
   ];
+  let content: any = launches.map((launch: any) => (
+    <Col span={8}>
+      <Card
+        style={{
+          borderRadius: "10px",
+          border: "1px solid #ccc",
+          margin: "5px 0px",
+          padding: "10px",
+        }}
+        cover={
+          <img
+            style={{
+              width: "50%",
+              height: "50%",
+              objectFit: "cover",
+              backgroundPosition: "center",
+              margin: "0 auto",
+            }}
+            src={launch.links.mission_patch_small}
+          />
+        }
+      >
+        <Card.Meta
+          style={{ textAlign: "center", padding: "0 10px" }}
+          title={launch.mission_name}
+          description={`${
+            launch?.details?.length > 20
+              ? launch.details?.slice(0, 35)
+              : launch.details || "No Details"
+          }...`}
+        />
+        <div>
+          <p>{}</p>
+        </div>
+      </Card>
+    </Col>
+  ));
+  if (filter?.status) {
+    content = launches
+      .filter((launch: any) => launch.launch_success)
+      .map((launch: any) => (
+        <Col span={8}>
+          <Card
+            style={{
+              borderRadius: "10px",
+              border: "1px solid #ccc",
+              margin: "5px 0px",
+              padding: "10px",
+            }}
+            cover={
+              <img
+                style={{
+                  width: "50%",
+                  height: "50%",
+                  objectFit: "cover",
+                  backgroundPosition: "center",
+                  margin: "0 auto",
+                }}
+                src={launch.links.mission_patch_small}
+              />
+            }
+          >
+            <Card.Meta
+              style={{ textAlign: "center", padding: "0 10px" }}
+              title={launch.mission_name}
+              description={`${
+                launch?.details?.length > 20
+                  ? launch.details?.slice(0, 35)
+                  : launch.details || "No Details"
+              }...`}
+            />
+            <div></div>
+          </Card>
+        </Col>
+      ));
+  }
+
+  if (filter?.upcoming) {
+    content = launches
+      .filter((launch: any) => launch?.upcoming)
+      .map((launch: any) => (
+        <Col span={8}>
+          <Card
+            style={{
+              borderRadius: "10px",
+              border: "1px solid #ccc",
+              margin: "5px 0px",
+              padding: "10px",
+            }}
+            cover={
+              <img
+                style={{
+                  width: "50%",
+                  height: "50%",
+                  objectFit: "cover",
+                  backgroundPosition: "center",
+                  margin: "0 auto",
+                }}
+                src={launch.links.mission_patch_small}
+              />
+            }
+          >
+            <Card.Meta
+              style={{ textAlign: "center", padding: "0 10px" }}
+              title={launch.mission_name}
+              description={`${
+                launch?.details?.length > 20
+                  ? launch.details?.slice(0, 35)
+                  : launch.details || "No Details"
+              }...`}
+            />
+            <div></div>
+          </Card>
+        </Col>
+      ));
+  }
+
+  if (filter?.status && filter?.upcoming) {
+    content = launches
+      .filter((launch: any) => launch.launch_success && launch?.upcoming)
+      .map((launch: any) => (
+        <Col span={8}>
+          <Card
+            style={{
+              borderRadius: "10px",
+              border: "1px solid #ccc",
+              margin: "5px 0px",
+              padding: "10px",
+            }}
+            cover={
+              <img
+                style={{
+                  width: "50%",
+                  height: "50%",
+                  objectFit: "cover",
+                  backgroundPosition: "center",
+                  margin: "0 auto",
+                }}
+                src={launch.links.mission_patch_small}
+              />
+            }
+          >
+            <Card.Meta
+              style={{ textAlign: "center", padding: "0 10px" }}
+              title={launch.mission_name}
+              description={`${
+                launch?.details?.length > 20
+                  ? launch.details?.slice(0, 35)
+                  : launch.details || "No Details"
+              }...`}
+            />
+            <div></div>
+          </Card>
+        </Col>
+      ));
+  }
   return (
     <div style={{ marginTop: "20px" }}>
       <div
@@ -40,57 +196,24 @@ const Home = () => {
           gap: "10px",
         }}
       >
-        <Dropdown menu={{ items }} placement="bottomLeft" arrow>
+        {/* <Dropdown menu={{ items }} placement="bottomLeft" arrow>
           <Button type="primary">Launch Date</Button>
-        </Dropdown>
+        </Dropdown> */}
         <Button
           type={filter?.status && "primary"}
           onClick={() => dispatch(setStatus(""))}
         >
           Status
         </Button>
-        <Button type="primary" onClick={() => dispatch(setUpcoming(""))}>
+        <Button
+          type={filter?.upcoming && "primary"}
+          onClick={() => dispatch(setUpcoming(""))}
+        >
           upcoming?
         </Button>
       </div>
       <Row style={{ width: "80%", margin: "10px auto" }} gutter={10}>
-        {launches.map((launch: any) => (
-          <Col span={8}>
-            <Card
-              style={{
-                borderRadius: "10px",
-                border: "1px solid #ccc",
-                margin: "5px 0px",
-                padding: "10px",
-              }}
-              cover={
-                <img
-                  style={{
-                    width: "50%",
-                    height: "50%",
-                    objectFit: "cover",
-                    backgroundPosition: "center",
-                    margin: "0 auto",
-                  }}
-                  src={launch.links.mission_patch_small}
-                />
-              }
-            >
-              <Card.Meta
-                style={{ textAlign: "center", padding: "0 10px" }}
-                title={launch.mission_name}
-                description={`${
-                  launch?.details?.length > 20
-                    ? launch.details?.slice(0, 35)
-                    : launch.details || "No Details"
-                }...`}
-              />
-              <div>
-                <p>{}</p>
-              </div>
-            </Card>
-          </Col>
-        ))}
+        {content}
       </Row>
     </div>
   );
